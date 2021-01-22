870e331d100c5286f5ec42d73f4751ca4cb6049f
xiaobo <xiaobo@bilibili.com>
2021-01-22 16:26:46 +0800

    data driven fixture

---
+++ cases/test_param_2_fixture.py
@@ -0,0 +1,19 @@
+import pytest
+
+# 测试账号数据
+test_user_data = ["admin1", "admin2"]
+
+
+@pytest.fixture(scope="module")
+def login(request):
+    user = request.param
+    print("pre:", user)
+    yield user
+    print("post:", user)
+
+
+@pytest.mark.parametrize("login", test_user_data, indirect=True)  # 添加indirect=True参数是为了把login当成一个函数去执行，而不是一个参数
+def test_login(login):
+    """test case"""
+    u = login
+    print("process", u)
